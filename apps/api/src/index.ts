2c629a1e60a583e9886024c12654361e2120b80a
Wilson Lima <wilson@codeindev.com>
2024-04-24 20:24:06 -0300

    setup do banco de dados

--- apps/api/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { defineAbilityFor } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER' })
-
-const userCanInviteSomeOnUser = ability.can('invite', 'User')
-const userCanDeleteOtherUsers = ability.can('delete', 'User')
-
-console.log(userCanInviteSomeOnUser)
-console.log(userCanDeleteOtherUsers)
