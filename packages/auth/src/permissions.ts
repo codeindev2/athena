e7f69cfda51530463e5874e7134db7cc774d1990
Wilson Lima <wilson@codeindev.com>
2024-04-24 17:51:50 -0300

    definida permissões

--- packages/auth/src/permissions.ts
+++ packages/auth/src/permissions.ts
@@ -10,13 +10,19 @@ type PermissionsByRole = (
 ) => void
 
 export const permissions: Record<Role, PermissionsByRole> = {
-  ADMIN(_, { can }) {
+  ADMIN(user, { can, cannot }) {
     can('manage', 'all') // Pode gerenciar todos os recursos
+    cannot(['transfer_ownership', 'update'], 'Organization') // Primeiro tirar a possibilidade de transferir a propriedade da organização
+    can(['transfer_ownership', 'update'], 'Organization', {
+      ownerId: { $eq: user.id },
+    }) // Depois dar permissão para transferir organizacao que pertence ao usuário
   },
   MEMBER(user, { can }) {
-    // can('invite', 'User') // Pode convidar usuários
-    can('manage', 'Project') // Pode gerenciar projetos
-    can(['update'], 'Project', { ownerId: { $eq: user.id } }) // Valida se o usuário é o dono do projeto
+    can('get', 'User') // Pode criar projetos
+    can(['get', 'create'], 'Project') // Pode gerenciar projetos
+    can(['update', 'delete'], 'Project', { ownerId: { $eq: user.id } }) // Valida se o usuário é o dono do projeto
   },
-  BILLING() {},
+  BILLING(user, { can }) {
+    can('manage', 'Billing')
+  },
 }
