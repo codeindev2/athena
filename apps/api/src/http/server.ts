56f93ac657ff4bae93c6001efe7596414608943d
Wilson Lima <wilson@codeindev.com>
2024-04-25 23:25:42 -0300

    configuração variaveis de ambiente

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -2,6 +2,7 @@ import fastifyCors from '@fastify/cors'
 import fastifyJwt from '@fastify/jwt'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUi from '@fastify/swagger-ui'
+import { env } from '@saas/env'
 import fastify from 'fastify'
 import {
   jsonSchemaTransform,
@@ -31,7 +32,15 @@ app.register(fastifySwagger, {
       description: 'Full-stack SaaS with multi-tenant & RBAC.',
       version: '1.0.0',
     },
-    servers: [],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
   },
   transform: jsonSchemaTransform,
 })
@@ -42,7 +51,7 @@ app.register(fastifySwaggerUi, {
 
 app.register(fastifyCors)
 app.register(fastifyJwt, {
-  secret: 'secret',
+  secret: env.JWT_SECRET,
 })
 app.register(createAccount)
 app.register(authenticateWithPassword)
@@ -51,6 +60,6 @@ app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
 })
