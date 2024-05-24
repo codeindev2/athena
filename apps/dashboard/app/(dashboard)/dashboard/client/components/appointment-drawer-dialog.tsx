a25bd9eed5b3f46db80cede8ea97f38c3636ca3b
Wilson Lucena <wilsonllucena@gmail.com>
2024-05-24 19:17:33 -0300

    mlehorias

--- apps/dashboard/app/(dashboard)/dashboard/client/components/appointment-drawer-dialog.tsx
+++ apps/dashboard/app/(dashboard)/dashboard/client/components/appointment-drawer-dialog.tsx
@@ -3,6 +3,12 @@ import * as React from "react";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import { AppointmentForm } from "./appointment-form";
 import { Modal } from "@/components/ui/modal";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+} from "@/components/ui/drawer";
 
 type AppointmentModalProps = {
   isOpen: boolean;
@@ -16,33 +22,33 @@ export function AppointmentDrawerDialog({
   onClose,
   clientId,
 }: AppointmentModalProps) {
-  // const isDesktop = useMediaQuery("(min-width: 768px)");
+  const isDesktop = useMediaQuery("(min-width: 768px)");
 
-  // if (isDesktop) {
+  if (isDesktop) {
+    return (
+      <Modal title="Agendamento" isOpen={isOpen} onClose={onClose}>
+        <AppointmentForm
+          clientId={clientId}
+          onClose={() => onClose()}
+          isOpen={isOpen}
+        />
+      </Modal>
+    );
+  }
+
   return (
-    <Modal title="Agendamento" isOpen={isOpen} onClose={onClose}>
-      <AppointmentForm
-        clientId={clientId}
-        onClose={() => onClose()}
-        isOpen={isOpen}
-      />
-    </Modal>
+    <Drawer open={isOpen} onClose={onClose}>
+      <DrawerContent>
+        <DrawerHeader className="text-left">
+          <DrawerTitle>Agendamento</DrawerTitle>
+        </DrawerHeader>
+        <AppointmentForm
+          className="px-4"
+          clientId={clientId}
+          onClose={onClose}
+          isOpen
+        />
+      </DrawerContent>
+    </Drawer>
   );
-  // }
-
-  // return (
-  //   <Drawer open={isOpen} onClose={onClose}>
-  //     <DrawerContent>
-  //       <DrawerHeader className="text-left">
-  //         <DrawerTitle>Agendamento</DrawerTitle>
-  //       </DrawerHeader>
-  //       <AppointmentForm
-  //         className="px-4"
-  //         clientId={clientId}
-  //         onClose={onClose}
-  //         isOpen
-  //       />
-  //     </DrawerContent>
-  //   </Drawer>
-  // );
 }
